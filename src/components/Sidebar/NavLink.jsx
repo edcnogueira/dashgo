a25d7b7051c20fdacb864888199f9f909f386d2e
Edjancarlos <edjan.nogueira14@gmail.com>
2022-06-09 23:21:34 -0300

    feat: ajusta exibição da página que está sendo acessada no momento

--- src/components/Sidebar/NavLink.jsx
+++ src/components/Sidebar/NavLink.jsx
@@ -1,14 +1,14 @@
 import { Icon, Link as ChakraLink, Text } from "@chakra-ui/react";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 
 export function NavLink({ icon, children, href, ...rest }) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">{children}</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   )
 }
