9889d3740868afd808c4d97421eaae9209372494
Edjancarlos <edcnogueira@pop-os.localdomain>
2022-06-19 21:26:55 -0300

    feat: adiciona o miragejs ao ambiente de desenvolvimento

---
+++ src/services/mirage/index.js
@@ -0,0 +1,22 @@
+import { createServer, Model } from "miragejs";
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend({}),
+    },
+
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
+
+      this.get("/users");
+      this.post("/users");
+
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
+
+  return server;
+}
